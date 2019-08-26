359f8405ad7159ab713f8231fff15424c7f44ecd
Petru Birzu <petru.birzu96@gmail.com>
2019-08-27 00:22:27 +0200

    removed web.sh

--- web.sh
+++
@@ -1,2 +0,0 @@
-# node index.js --name="egn-web(staging)" --method="pm2" --host="netvaerk.com" --user="web" --password="vFp1T\$iT" --cwd="/home/web/public_html" --destination="dist" --local-path="/Users/petru/Development/Wiredelta/egn-web/dist"
-wd-deployer --name="egn-web(staging)" --method="pm2" --host="netvaerk.com" --user="web" --password="vFp1T\$iT" --cwd="/home/web/public_html" --destination="dist" --local-path="/Users/petru/Development/Wiredelta/egn-web/dist"
